77bea513140f030b33084a8766b26aa837af0f13
lakshaynanda <lakshay.nanda08@gmail.com>
2021-10-02 11:41:56 -0700

    Changed to VUE3

--- csg/src/main.js
+++ csg/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+createApp(App).mount('#app')
