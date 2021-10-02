fcc66bb83869b06ca4503702dcb735077f7e5de1
lakshaynanda <lakshay.nanda08@gmail.com>
2021-10-02 12:27:32 -0700

    Updates to files

--- csg/src/App.vue
+++ csg/src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <Form msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import Form from './components/Form.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Form
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- csg/src/main.js
+++ csg/src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
